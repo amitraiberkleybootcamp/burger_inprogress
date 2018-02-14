da4459578fd22df0b99fbe5a7616873cfce26a48
Amit Rai <amit.rai.berkleybootcamp@gmail.com>
2018-02-14 13:57:06 -0800

    WIP

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lettuce Wrap Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Veggie Burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meatless Beet Burgers', false);
